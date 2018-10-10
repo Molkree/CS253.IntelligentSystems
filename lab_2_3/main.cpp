2af11bd29e798cd1370b1dbaaa45cd34a17711cf
numumotik <numumotik@mail.ru>
2018-10-10 23:19:53 +0300

    конструктор

--- lab_2_3/main.cpp
+++ lab_2_3/main.cpp
@@ -1,4 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
 
+const int WOLF_W = 2;
+const int SHEEP_W = 1;
+const int NOTHING = 0;
+using namespace std;
+
+vector<int> dx{ 1, 1, -1, -1 };
+vector<int> dy{ 1, -1, -1, 1 };
+
+class Node
+{
+public:
+
+	vector<pair<uint_fast8_t, uint_fast8_t>> wolves;
+	pair<uint_fast8_t, uint_fast8_t> sheep;
+	
+	//sheep - 1, wolfs - 2, nothing - 0
+	int terminal()
+	{
+		if (sheep.second == 7)
+			return 1;
+
+		bool dead = true;
+		for (int i = 0; i < 4; ++i)
+		{
+			int pos_x = sheep.first + dx[i];
+			int pos_y = sheep.second + dy[i];
+			if (pos_x < 0 || pos_x > 7 || pos_y < 0 || pos_y > 7)
+				continue;
+			for (int j = 0; j<4; ++j)
+				if (wolves[j].first != pos_x || wolves[j].second != pos_y)
+				{
+					dead = false;
+					break;
+				}
+			if (!dead) break;
+		}
+		if (dead) return 2;
+		return 0;
+	}
+
+	Node(const pair<uint_fast8_t, uint_fast8_t>& w1, const pair<uint_fast8_t, uint_fast8_t>& w2, const pair<uint_fast8_t, uint_fast8_t>& w3, 
+		const pair<uint_fast8_t, uint_fast8_t>& w4, const pair<uint_fast8_t, uint_fast8_t>& sheep)
+	{
+		wolves.push_back(w1);
+		wolves.push_back(w2);
+		wolves.push_back(w3);
+		wolves.push_back(w4);
+		this->sheep = sheep;
+	}
+
+private:
+
+};
 
 int main()
 {
