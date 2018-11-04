3b9065fb9891b469e9b894d9b06b3e6f5a4a1a1e
Taally <shkuro_tanya@mail.ru>
2018-11-04 17:25:16 +0300

    продукционная модель, прямой вывод

---
+++ lab4/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.list_villains = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_heros = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label_info = new System.Windows.Forms.Label();
+            this.button_exec = new System.Windows.Forms.Button();
+            this.check_forward = new System.Windows.Forms.RadioButton();
+            this.check_backward = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Команда злодеев";
+            // 
+            // list_villains
+            // 
+            this.list_villains.FormattingEnabled = true;
+            this.list_villains.Location = new System.Drawing.Point(13, 37);
+            this.list_villains.Name = "list_villains";
+            this.list_villains.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.list_villains.Size = new System.Drawing.Size(120, 95);
+            this.list_villains.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(334, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Команда героев";
+            // 
+            // label_heros
+            // 
+            this.label_heros.AutoSize = true;
+            this.label_heros.BackColor = System.Drawing.Color.White;
+            this.label_heros.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label_heros.Location = new System.Drawing.Point(337, 37);
+            this.label_heros.Name = "label_heros";
+            this.label_heros.Size = new System.Drawing.Size(55, 15);
+            this.label_heros.TabIndex = 3;
+            this.label_heros.Text = "Никого...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Информация о выводе";
+            // 
+            // label_info
+            // 
+            this.label_info.AutoSize = true;
+            this.label_info.BackColor = System.Drawing.Color.White;
+            this.label_info.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label_info.Location = new System.Drawing.Point(13, 271);
+            this.label_info.Name = "label_info";
+            this.label_info.Size = new System.Drawing.Size(18, 15);
+            this.label_info.TabIndex = 5;
+            this.label_info.Text = "...";
+            // 
+            // button_exec
+            // 
+            this.button_exec.Location = new System.Drawing.Point(13, 195);
+            this.button_exec.Name = "button_exec";
+            this.button_exec.Size = new System.Drawing.Size(120, 23);
+            this.button_exec.TabIndex = 6;
+            this.button_exec.Text = "Собрать команду";
+            this.button_exec.UseVisualStyleBackColor = true;
+            this.button_exec.Click += new System.EventHandler(this.button_exec_Click);
+            // 
+            // check_forward
+            // 
+            this.check_forward.AutoSize = true;
+            this.check_forward.Checked = true;
+            this.check_forward.Location = new System.Drawing.Point(12, 139);
+            this.check_forward.Name = "check_forward";
+            this.check_forward.Size = new System.Drawing.Size(100, 17);
+            this.check_forward.TabIndex = 7;
+            this.check_forward.TabStop = true;
+            this.check_forward.Text = "Прямой вывод";
+            this.check_forward.UseVisualStyleBackColor = true;
+            // 
+            // check_backward
+            // 
+            this.check_backward.AutoSize = true;
+            this.check_backward.Location = new System.Drawing.Point(12, 162);
+            this.check_backward.Name = "check_backward";
+            this.check_backward.Size = new System.Drawing.Size(111, 17);
+            this.check_backward.TabIndex = 8;
+            this.check_backward.Text = "Обратный вывод";
+            this.check_backward.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 459);
+            this.Controls.Add(this.check_backward);
+            this.Controls.Add(this.check_forward);
+            this.Controls.Add(this.button_exec);
+            this.Controls.Add(this.label_info);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label_heros);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.list_villains);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox list_villains;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_heros;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label_info;
+        private System.Windows.Forms.Button button_exec;
+        private System.Windows.Forms.RadioButton check_forward;
+        private System.Windows.Forms.RadioButton check_backward;
+    }
+}
+
---
+++ lab4/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        List<Fact> given_facts = new List<Fact>();
+        List<Fact> all_facts = new List<Fact>();
+        List<Terminal> terminals = new List<Terminal>();
+        List<Rule> rules = new List<Rule>();
+        static FactComparer cmp = new FactComparer();
+
+        HashSet<Fact> known_facts = new HashSet<Fact>(cmp);
+
+//        List<Fact> known_facts = new List<Fact>(); // proven facts
+        List<Fact> support_area = new List<Fact>(); // non terminal
+            
+        //private List<Fact> possible_knoweledge = new List<Fact>(); //all facts that users can change
+        //private List<TerminalFact> terminals = new List<TerminalFact>(); // terminal facts
+        //private List<Rule> Rules = new List<Rule>();
+        //private HashSet<Fact> work_area = new HashSet<Fact>(); // proven facts
+        //private HashSet<Fact> support_area = new HashSet<Fact>();  // non terminal 
+        public Form1()
+        {
+            InitializeComponent();
+            read_data("../../database.txt");
+            init_listbox();
+        }
+
+        public void read_data(string filename)
+        {
+            List<string> lines = System.IO.File.ReadLines(filename, Encoding.UTF8).ToList();
+            
+            // f
+            var str = lines[0].Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            int facts_cnt = Int32.Parse(str[0]);
+            for (int i = 1; i < facts_cnt + 1; ++i)
+            {
+                str = lines[i].Split(new char[] { ':', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                Fact f = new Fact(str[0].Trim(), str[1]);
+                all_facts.Add(f);
+            }
+
+            // s
+            str = lines[facts_cnt + 1].Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            int support_cnt = Int32.Parse(str[0]);
+            int n = facts_cnt + support_cnt + 1;
+            for (int i = facts_cnt + 2; i < n; ++i)
+            {
+                str = lines[i].Split(new char[] { ':', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                all_facts.Add(new Fact(str[0].Trim(), str[1]));
+            }
+
+            // t
+            str = lines[n].Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            int term_cnt = Int32.Parse(str[0]);
+            n += 1;
+            for (int i = n; i < n + term_cnt; ++i)
+            {
+                str = lines[i].Split(new char[] { ':', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                all_facts.Add(new Terminal(str[0].Trim(), str[1]));
+                terminals.Add(new Terminal(str[0].Trim(), str[1]));
+            }
+
+            // r
+            n += term_cnt;
+            str = lines[n].Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            int rules_cnt = Int32.Parse(str[0]);
+            for (int i = n + 1; i < n + rules_cnt; ++i)
+            {
+                str = lines[i].Split(new char[] { ':', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                string id = str[0].Trim();
+
+                // condition
+                var fact_str = str[1].Split(new char[] { ',', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                List<Fact> cond = new List<Fact>();
+                for (int j = 0; j < fact_str.Count(); ++j)
+                {
+                    string index = fact_str[j].Trim();
+                    Fact fact = all_facts.Find(f => f.id.Equals(index));
+
+                    if (fact.id != "") // exists
+                        cond.Add(fact);
+                }
+
+                // result
+                fact_str = str[2].Split(new char[] { ',', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                Fact res = all_facts.Find(f => f.id.Equals(fact_str[0].Trim()));
+
+                if (res.id != "") // exists
+                {
+                    rules.Add(new Rule(id, cond, res));
+                }
+
+
+            }
+        }
+
+        public void init_listbox()
+        {
+            foreach (Fact f in all_facts)
+            {
+                if (f.id[0] != 'f')
+                    break;
+                list_villains.Items.Add(f.info);
+            }
+        }
+
+        private void button_exec_Click(object sender, EventArgs e)
+        {
+            foreach (int ind in list_villains.SelectedIndices)
+            {
+                Fact fact = all_facts.Find(f => f.info == (list_villains.Items[ind]).ToString());
+                if (fact.id != "")
+                {
+                    given_facts.Add(fact);
+                }
+            }
+            label_heros.Text = "";
+
+            if (check_forward.Checked)
+                forward_reasoning();
+            else
+                backward_reasoning();
+        }
+
+        public void forward_reasoning()
+        {
+            foreach (Fact fact in given_facts)
+                known_facts.Add(fact);
+
+            while (true)
+            {
+                int prev_cnt = known_facts.Count;
+                foreach (Rule r in rules)
+                {
+                    bool cond = true;
+                    foreach (Fact f in r.condition)
+                        if (!known_facts.Contains(f, cmp))
+                        {
+                            cond = false;
+                            break;
+                        }
+                    if (cond)
+                    {
+                        known_facts.Add(r.result);
+                    }
+                }
+                if (prev_cnt == known_facts.Count)
+                    break;
+            }
+
+            foreach (Fact f in known_facts)
+            {
+                if (f.id[0] == 't')
+                    label_heros.Text += f.info + "\n";
+            }
+
+        
+        }
+
+        public void backward_reasoning()
+        {
+            
+
+  /*          HashSet<Fact> targets = new HashSet<Fact>(cmp);
+            foreach (Fact fact in terminals)
+            {
+                targets.Add(fact);
+            }
+
+            //foreach (Fact fact in terminals)
+            //    known_facts.Add(fact);
+
+            while (true)
+            {
+                foreach (Rule r in rules)
+                {
+                    if (targets.Contains(r.result, cmp))
+                    {
+                        foreach (Fact cond in r.condition)
+                        {
+                            if (!known_facts.Contains(cond, cmp))
+                                targets.Add(cond);
+                        }
+
+                    }
+                }
+            }*/
+        }
+
+    }
+
+    public class Fact
+    {
+        public string id = "";
+        public string info = "";
+
+        public Fact() { }
+
+        public Fact(string id, string info)
+        {
+            this.id = id.Trim();
+            this.info = info;
+        }
+
+    /*    public bool Equals(Fact obj)
+        {
+            if (obj == null) return false;
+            return id.Equals(obj.id);
+        }*/
+    }
+
+    public class FactComparer : IEqualityComparer<Fact>
+    {
+
+        public bool Equals(Fact f1, Fact f2)
+        {
+            return (f1.id.Equals(f2.id));
+        }
+
+        public int GetHashCode(Fact f)
+        {
+            return f.id.GetHashCode() ^ f.info.GetHashCode();
+        }
+    }
+
+    public class Terminal: Fact
+    {
+        public Terminal() { }
+
+        public Terminal(string id, string info)
+        {
+            this.id = id.Trim();
+            this.info = info;
+        }
+    }
+
+    public class Rule
+    {
+        public string id = "";
+        public List<Fact> condition;
+        public Fact result;
+        public string info = "";
+
+        public Rule()
+        {
+            condition = new List<Fact>();
+            result = new Fact();          
+        }
+        public Rule(string id, List<Fact> cond, Fact res)
+        {
+            this.id = id;
+            condition = new List<Fact>(cond);
+            result = res;
+
+            info = id + ": " + string.Join(", ", condition.Select(f => f.info)) + " => " + string.Join(", ", res.info);
+        }
+
+   /*     public bool Equals(Rule obj)
+        {
+            if (obj == null) return false;
+            return id.Equals(obj.id);
+        }*/
+    }
+    public class RuleComparer : IEqualityComparer<Rule>
+    {
+
+        public bool Equals(Rule r1, Rule r2)
+        {
+            return (r1.id.Equals(r2.id));
+        }
+
+        public int GetHashCode(Rule r)
+        {
+            return r.id.GetHashCode() ^ r.info.GetHashCode();
+        }
+    }
+
+    class Node
+    {
+        public List<Node> parents = new List<Node>();
+        public List<Node> children = new List<Node>();
+
+        public Node() { }
+    }
+
+    class AndNode: Node
+    {
+        public Rule r = new Rule();
+        public AndNode() { }
+        public AndNode(Rule rule)
+        {
+            r = rule;
+        }
+    }
+
+    class OrNode : Node
+    {
+        public Fact f = new Fact();
+        public OrNode() { }
+        public OrNode(Fact fact)
+        {
+            f = fact;
+        }
+    }
+}
