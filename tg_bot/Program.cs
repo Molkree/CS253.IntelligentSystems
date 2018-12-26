b7020327d19346280fca8a973ea055d425d9daf5
Dmitry Volodin <mr.molkree@gmail.com>
2018-12-26 05:36:43 +0300

    commands and more questions/answers added logging

--- tg_bot/Program.cs
+++ tg_bot/Program.cs
@@ -4,7 +4,7 @@ using Telegram.Bot;
 using Telegram.Bot.Args;
 using AIMLbot;
 using System.Linq;
-using System.Net;
+using System.IO;
 
 namespace Awesome
 {
@@ -14,6 +14,9 @@ namespace Awesome
         static readonly Bot AI = new Bot(); // This defines the object "AI" To hold the bot's infomation
         static User myUser;
 
+        static bool silenced = false;
+        static bool read_everything = false;
+
         static void Main()
         {
             AI.loadSettings(); // This loads the settings from the config folder
@@ -22,8 +25,8 @@ namespace Awesome
             myUser = new User("Username", AI); // This creates a new User called "Username", using the object "AI"'s information.
             AI.isAcceptingUserInput = true; // This swithces the user input back on
 
-            WebProxy myproxy = new WebProxy("189.90.248.75", 8080);
-            botClient = new TelegramBotClient("token", myproxy);
+            //WebProxy myproxy = new WebProxy("189.90.248.75", 8080);
+            botClient = new TelegramBotClient("token"/*, myproxy*/);
 
             var me = botClient.GetMeAsync().Result;
             Console.WriteLine(
@@ -41,70 +44,70 @@ namespace Awesome
             {
                 bool mention = e.Message.EntityValues != null && e.Message.EntityValues.Contains("@team99_bot");
                 bool reply = e.Message.ReplyToMessage != null && e.Message.ReplyToMessage.From.Username == "team99_bot";
-                if (!mention && !reply)
-                    return;
-
-                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
 
                 var input = e.Message.Text;
+                if (!silenced && mention && input.StartsWith("SILENCE") || !silenced && input.StartsWith("SILENCE_ALL"))
+                {
+                    silenced = true;
+                    Console.WriteLine($"Silenced in chat {e.Message.Chat.Id} at {e.Message.Date}.");
+                    return;
+                }
+                if (silenced && mention && input.StartsWith("UNSILENCE") || silenced && input.StartsWith("UNSILENCE_ALL"))
+                {
+                    silenced = false;
+                    Console.WriteLine($"Unsilenced in chat {e.Message.Chat.Id} at {e.Message.Date}.");
+                    return;
+                }
+                if (!silenced && mention && input.StartsWith("REPORT") || !silenced && input.StartsWith("REPORT_ALL"))
+                {
+                    await botClient.SendTextMessageAsync(
+                        replyToMessageId: e.Message.MessageId,
+                        chatId: e.Message.Chat,
+                        text: "Я жив. А ты?"
+                        );
+                    Console.WriteLine($"Reported in chat {e.Message.Chat.Id} at {e.Message.Date}.");
+                    return;
+                }
+                if (mention && input.StartsWith("BLABER_ON") || input.StartsWith("BLABER_ON_ALL"))
+                {
+                    read_everything = true;
+                    Console.WriteLine($"BLABER_ON in chat {e.Message.Chat.Id} at {e.Message.Date}.");
+                    return;
+                }
+                if (mention && input.StartsWith("BLABER_OFF") || input.StartsWith("BLABER_OFF_ALL"))
+                {
+                    read_everything = false;
+                    Console.WriteLine($"BLABER_OFF in chat {e.Message.Chat.Id} at {e.Message.Date}.");
+                    return;
+                }
+
+                if (!mention && !reply && !read_everything || silenced)
+                    return;
+
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id} at {e.Message.Date}.");
+
+                using (StreamWriter sw = File.AppendText("../../../log.txt"))
+                {
+                    sw.WriteLine(e.Message.Date);
+                    sw.WriteLine(e.Message.From.Username + ": " + input);
+                }
                 if (mention)
-                    input = input.Remove(0, 12); // remove mention
+                    input = input.Replace("@team99_bot", ""); // remove mention
                 Request r = new Request(input, myUser, AI); // This generates a request using text from message, the user and the AI object's.
                 Result res = AI.Chat(r); // This sends the request off to the object AI to get a reply back based of the AIML file's.
 
                 await botClient.SendTextMessageAsync(
                     replyToMessageId: e.Message.MessageId,
                     chatId: e.Message.Chat,
-                    text: res.Output
+                    text: res.Output == "" ? "Я не понял!!!!!!!" : res.Output
                     );
+
+                using (StreamWriter sw = File.AppendText("../../../log.txt"))
+                {
+                    sw.WriteLine(res.Output == "" ? "Я не понял!!!!!!!" : res.Output);
+                    sw.WriteLine();
+                }
             }
-                //Message msg1 = await botClient.SendStickerAsync(
-                //    chatId: e.Message.Chat,
-                //    sticker: "https://github.com/TelegramBots/book/raw/master/src/docs/sticker-fred.webp"
-                //    );
-                //Message msg2 = await botClient.SendStickerAsync(
-                //    chatId: e.Message.Chat,
-                //    sticker: msg1.Sticker.FileId
-                //    );
-
-                //Message message = await botClient.SendPhotoAsync(chatId: e.Message.Chat,
-                //    photo: "https://github.com/TelegramBots/book/raw/master/src/docs/photo-ara.jpg",
-                //    caption: "<b>Ara bird</b>. <i>Source</i>: <a href=\"https://pixabay.com\">Pixabay</a>",
-                //    parseMode: ParseMode.Html
-                //    );
-
-                //Message message = await botClient.SendTextMessageAsync(
-                //  chatId: e.Message.Chat, // or a chat id: 123456789
-                //  text: "Trying *all the parameters* of `sendMessage` method",
-                //  parseMode: ParseMode.Markdown,
-                //  disableNotification: true,
-                //  replyToMessageId: e.Message.MessageId,
-                //  replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithUrl(
-                //    "Check sendMessage method",
-                //    "https://core.telegram.org/bots/api#sendmessage"
-                //  ))
-                //);
-                //Console.WriteLine(
-                //    $"{message.From.FirstName} sent message {message.MessageId} " +
-                //    $"to chat {message.Chat.Id} at {message.Date}. " +
-                //    $"It is a reply to message {message.ReplyToMessage.MessageId} " +
-                //    $"and has {message.Entities.Length} message entities."
-                //    );
-
-                //await botClient.SendStickerAsync(
-                //    chatId: e.Message.Chat,
-                //    sticker: "https://github.com/TelegramBots/book/raw/master/src/docs/sticker-dali.webp"
-                //    );
-
-                //await botClient.SendTextMessageAsync(
-                //    chatId: e.Message.Chat,
-                //    text: "Hello, World!"
-                //    );
-
-                //await botClient.SendTextMessageAsync(
-                //  chatId: e.Message.Chat,
-                //  text: "You said:\n" + e.Message.Text
-                //);
-            }
+        }
     }
 }
